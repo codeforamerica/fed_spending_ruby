f09b692055bfc25f6eda4f0a1330d7ab6618cd87
blakeshall <blake@blakeshall.com>
2011-06-21 10:50:14 -0700

    Added fdps spec

--- spec/fed_spending/api_spec.rb
+++ spec/fed_spending/api_spec.rb
@@ -6,4 +6,19 @@ describe FedSpending::Client::Api do
     @client = FedSpending::Client.new
   end
   
+  describe ".fpds" do
+    before do
+      stub_get("fpds/fdps.php?datatype=X&detail=-1&state=AL").
+        to_return(:status => 200, :body => fixture("fedspending_search_results.xml"))
+    end
+    
+    it "Should return the correct item" do
+      test = @client.fdps()
+      a_get("fpds/fdps.php?datatype=X&detail=-1&state=AL").should have_been_made
+      test.fedspendingSearchResults.data.record.totals.rank_among_states.should == "20"
+    end
+  end
+    
+    
+  
 end
