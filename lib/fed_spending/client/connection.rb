2eed4b93702ea526dd3aa8e9a6ee9e284e3e617b
Justin Stoller <justin.stoller@gmail.com>
2011-06-22 10:37:05 -0700

    commented out xml parsing while debugging

--- lib/fed_spending/client/connection.rb
+++ lib/fed_spending/client/connection.rb
@@ -10,7 +10,7 @@ module FedSpending
           connection.use Faraday::Request::UrlEncoded
           connection.use Faraday::Response::RaiseError
           connection.use Faraday::Response::Rashify
-          connection.use Faraday::Response::ParseXml
+          #connection.use Faraday::Response::ParseXml
           #connection.use Faraday::Response::ParseJson
           connection.adapter(Faraday.default_adapter)
         end
